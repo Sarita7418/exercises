ffbd655e68c4fded9475ce18966486fdfeeee672
Sarita731 <mg6667418@gmail.com>
2024-12-07 09:37:14 -0400

    Header-terminado

---
+++ components/Exercise.tsx
@@ -0,0 +1,23 @@
+import Image, { StaticImageData } from "next/image";
+import flecha from "@/public/Icons.svg";
+import "./Exercise.css";
+
+interface ExerciseProps {
+  titulo: string;
+  imagen: StaticImageData;
+}
+
+const Exercise: React.FC<ExerciseProps> = ({ titulo, imagen }) => {
+  return (
+    <div>
+      <Image src={imagen} alt={titulo} />
+      <div>
+        <h2>{titulo}</h2>
+        <span>3 séries x 12 repetições</span>
+      </div>
+      <img src={flecha.src} alt="" />
+    </div>
+  );
+};
+
+export default Exercise;
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <span>Exercícios</span>
+        <span>4</span>
+    </div>
+  )
+}
+
+export default Header
